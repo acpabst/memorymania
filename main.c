74c7614f42228085e52ff1b608a3097910c5c238
Andy Pabst <andy.pabst16@gmail.com>
2024-06-18 06:56:14 +0000

    timer and debugging code added

--- main.c
+++ main.c
@@ -5,9 +5,78 @@
 
 #include "memorymania.h"
 
+bool game_over = false;
+bool caught_sigalrm = false;
+bool caught_sigusr = false;
+
+static void signal_handler(int signal_number) {
+    if (signal_number == SIGALRM) {
+	caught_sigalrm = true;
+	game_over = true;
+    }
+    if (signal_number == SIGUSR1) {
+	caught_sigusr = true;
+    }
+}
+
+void set_signal_handling() {
+    struct sigaction new_action;
+    memset(&new_action, 0, sizeof(struct sigaction));
+    new_action.sa_handler = signal_handler; 
+    int sa = sigaction(SIGALRM, &new_action, NULL);
+    if (sa != 0) {
+        syslog(LOG_ERR, "Failed to register for SIGALRM. Error: %d", sa);
+        exit(-1);
+    }
+    sa = sigaction(SIGUSR1, &new_action, NULL);
+    if (sa != 0) {
+        syslog(LOG_ERR, "Failed to register for SIGUSR. Error: %d", sa);
+        exit(-1);
+    }
+
+    syslog(LOG_INFO, "Signal Handling set up complete.");
+    printf("Signal handling set up.\n");
+}
+
+void game_timer() {
+    time_t t;
+    int r;
+    int num_char;
+    struct itimerval timer_spec;
+
+    timer_spec.it_interval.tv_sec = 10;
+    timer_spec.it_interval.tv_usec = 0;
+    timer_spec.it_value.tv_sec = 10;
+    timer_spec.it_value.tv_usec = 0;
+    
+    r = setitimer(ITIMER_REAL, &timer_spec, 0);
+    if (r != 0 ) {
+        syslog(LOG_ERR, "Failed to create timer. Error: %d", errno);
+	printf("Failed to create timer. Error: %d\n", errno);
+	exit(-1);
+    }
+
+    printf("In timer. Alarm should be set.\n");
+    
+    while(!game_over) {
+	sleep(0.01);
+        if(caught_sigalrm) {
+	    printf("Sigalarm caught. Time is up!\n");
+	    break;
+	}
+	if(caught_sigusr) {
+	   printf("Sigusr caught. Resetting timer.\n");
+           r = setitimer(ITIMER_REAL, &timer_spec, 0);
+	   caught_sigusr = false;
+	}
+    }
+    printf("Exiting Timer Process. . . \n");
+    exit(0);
+}
+
 // adapted for this game from jstest 
 char getButtonPress(int fd, unsigned char *buttons) {
-
+    
     printf("Getting button press\n");
     char button;
     //int i;
@@ -28,12 +97,10 @@ char getButtonPress(int fd, unsigned char *buttons) {
             printf("Something went wrong");	
             return 1;
         }
-	//printf("Made it here 3\n");
-	//printf("type: %i\n", js.type);
-	//printf("value: %i\n", js.value);
-	//printf("number: %i\n", js.number);
-	
-
+	if (caught_sigalrm) {
+	    // time is up, return dummy character
+            return 'N';
+	}
         if (js.type == JS_EVENT_BUTTON && js.value == 1) {
 	   waiting = false;
            /* button[js.number] = js.value;
@@ -85,13 +152,16 @@ char getButtonPress(int fd, unsigned char *buttons) {
 int main (int argc, char*argv[]) {
 
     int fd;
-    bool game_over = false;
+    int r;
     int score = 0;
 
     unsigned char buttons = 2;
     int version = 0x000800;
 
     char test_buffer[] = {'N','N','N','N','N','N','N','N','N','N','N'};
+
+    openlog(NULL, 0, LOG_USER); 
+    set_signal_handling();
 
     // open controller
     printf("Opening device: %s\n", argv[argc - 1]);
@@ -101,9 +171,18 @@ int main (int argc, char*argv[]) {
     }
     printf("Welcome to MEMORY MANIA!!\n");
     printf("Try to remember the sequence! The max score on today's game is %i.\n", MAX_SCORE);
-    //printf("Made it here\n");
     ioctl(fd, JSIOCGVERSION, &version);
     ioctl(fd, JSIOCGBUTTONS, &buttons);
+
+    // begin timer
+    printf("Creating timer . . .\n");
+    r = fork();
+    if (r < 0) {
+        printf("Error creating timer fork: %d\n", errno);
+	exit(-1);
+    } else if (r == 0) {
+	game_timer();
+    }
 
     // generate the first character
     int index_max = sizeof(supported_chars) - 1;
@@ -117,14 +196,18 @@ int main (int argc, char*argv[]) {
 	printf("score: %i\n", score);
 	for (int i = 0; i <= score; i++) {
 	    printf("i: %i\n", i);
-	    //char input = getchar();
-	    char input = getButtonPress(fd, &buttons);
+	    char input = getInput(fd, &buttons);
+	    if (caught_sigalrm) {
+		    printf("Something is happening. . .\n");
+	    }
 	    printf("You said: %c\n", input);
 	    if (input == test_buffer[i]) {
 	        printf("Correct!\n");
+		kill(r, SIGUSR1);
 	    } else {
 		printf("Incorrect :(\n");
 		game_over = true;
+		kill(r, SIGALRM);
 		break;
             }
 	}
@@ -134,6 +217,10 @@ int main (int argc, char*argv[]) {
 	    game_over = true;
 	    break;
         }
+
+	if (MEMORYMANIA_DEBUG) {
+	    getchar(); // get carriage return
+	}
         
         // generate new character
 	int index_max = sizeof(supported_chars) - 1;
--- memorymania.h
+++ memorymania.h
@@ -11,9 +11,25 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <pthread.h>
+#include <syslog.h>
+#include <string.h>
 
 // Supported Characters
 char supported_chars[] = {'A', 'B', 'X', 'Y', 'L', 'R'};
+
+//#define MEMORYMANIA_DEBUG 1 
+
+#ifdef MEMORYMANIA_DEBUG
+   /* This one for keyboard input */
+#  define getInput(args...) fgetc(stdin)
+#else
+    /* This one for XBOX Controller input*/
+#  define getInput(args...) getButtonPress(## args)
+#endif
+
 
 #define MAX_SCORE 10
 
